eaf7c3f89a002306d9d3db7f88306784f2d0b388
Abhishek KC <contact@kcabhishek.com.np>
2020-05-16 23:50:45 +0545

    Added scrolling effect to navigation bar links and hero buttons

--- res/js/scripts.js
+++ res/js/scripts.js
@@ -9,4 +9,36 @@ $(document).ready(function () {
     }, {
         offset: "60px"
     });
+
+    $('.js--scroll-to-plans').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000);
+    });
+    $('.js--scroll-to-start').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000);
+    });
+
+    $('.js--nav-delivery').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000);
+    });
+    $('.js--nav-steps').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.js--section-steps').offset().top
+        }, 1000);
+    });
+    $('.js--nav-cities').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.js--section-cities').offset().top
+        }, 1000);
+    });
+    $('.js--nav-sign-up').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.js--section-form').offset().top
+        }, 1000);
+    });
 });
